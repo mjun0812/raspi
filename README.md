58e63e7b36c00a50710241bb891e2ed69855533d
Junya Morioka <mjun@mjunya.com>
2022-11-04 17:21:59 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# My Raspberry Pi Zero 2 W repository
+
+## Install
+
+```bash
+./setup.sh
+sudo pip install cgsensor mh-z19
+```
+
+## Sensor
+
+- BME280: temperature, humidity, air pressure
+- TSL2572: Brightness
+- MH-Z19C: CO2
+
+```bash
+sudo python sensor.py
+```
+
---
+++ sensor.py
@@ -0,0 +1,25 @@
+import cgsensor
+import mh_z19
+from pprint import pprint
+
+
+def main():
+    result = {}
+
+    bme280 = cgsensor.BME280(i2c_addr=0x76)
+    bme280.forced()
+    result["温度"] = bme280.temperature
+    result["室温"] = bme280.humidity
+    result["気圧"] = bme280.pressure
+
+    tsl2572 = cgsensor.TSL2572()
+    tsl2572.single_auto_measure()
+    result["照度"] = tsl2572.illuminance
+
+    result["CO2"] = mh_z19.read()["co2"]
+
+    pprint(result)
+
+
+if __name__ == "__main__":
+    main()
