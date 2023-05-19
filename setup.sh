6276d5add9b56927688d70ab427b0ba6f5c631cf
Junya Morioka <mjun@mjunya.com>
2023-05-19 19:42:39 +0900

    update

--- setup.sh
+++ setup.sh
@@ -4,5 +4,7 @@ sudo apt update && sudo apt install -y vim
 
 sudo pip install -r requirements.txt
 
-sudo raspi-config
+sudo raspi-config nonint do_i2c 0
+sudo raspi-config nonint do_vnc 0
+sudo raspi-config nonint do_spi 0
 
